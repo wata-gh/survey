dfe6cdc8404062f96fa52f4db57a43a275971212
wata <wata.gm@gmail.com>
2015-09-29 18:06:55 +0900

    add cloudfront to production.

---
+++ app/uploaders/uploader_base.rb
@@ -0,0 +1,18 @@
+class UploaderBase < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+  storage :fog
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+end
