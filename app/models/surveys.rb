06fb16d76d3f4e741037725db04dc24d489f331f
wata <wata.gm@gmail.com>
2015-10-04 10:46:49 +0900

    refactor for code climate.

--- app/models/surveys.rb
+++ app/models/surveys.rb
@@ -4,6 +4,13 @@ class Surveys < ActiveRecord::Base
   has_many :collaborators, :dependent => :destroy
   scope :my_collab, ->(uuid) { where(collaborators: {uuid: uuid}) }
   scope :current_group, ->(subdomain) { joins(:group).where(groups: {name: subdomain}) }
+  scope :my_answer, ->(params, uuid) {
+    eager_load(:questions, :collaborators).find_by({
+      id: params[:survey_id],
+      questions:     {id:   params[:question_id]},
+      collaborators: {uuid: uuid},
+    })
+  }
   validates_presence_of :name
   validates_presence_of :questions, :on => :update
   paginates_per 10
