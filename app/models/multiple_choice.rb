0de5b08f81763e9052e0f9ee708877f88184592c
wata <wata.gm@gmail.com>
2015-10-04 14:24:13 +0900

    refactor for code climate

--- app/models/multiple_choice.rb
+++ app/models/multiple_choice.rb
@@ -1,14 +1,14 @@
 class MultipleChoice < Question
   def sel_sum
     s = Hash.new 0
-    self.answers.joins(:collaborator).where(collaborators: {status: 1}).pluck(:text).each do |t|
+    answers.joins(:collaborator).where(collaborators: {status: 1}).pluck(:text).each do |t|
       JSON.parse(t).each {|v| s[v] += 1}
     end
-    self.choices.inject [] {|r, c| r << s[c[:value]]; r}
+    choices.inject [] {|r, c| r << s[c[:value]]; r}
   end
 
   def value_data
-    JSON.parse(self.value)
+    JSON.parse value
   end
 
 end
